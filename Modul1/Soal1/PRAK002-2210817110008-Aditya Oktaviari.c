65d5da210aab9a5a08b37d0b8a4529db18009241
aditokta <115124814+aditokta@users.noreply.github.com>
2022-10-16 00:36:42 +0800

    Add files via upload

---
+++ Modul1/Soal1/PRAK002-2210817110008-Aditya Oktaviari.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Selamat Pagi, Aditya Oktaviari\n");
+    printf("Selamat Siang, Aditya Oktaviari\n");
+    printf("Selamat Malam, Aditya Oktaviari");
+
+    return 0;
+}
