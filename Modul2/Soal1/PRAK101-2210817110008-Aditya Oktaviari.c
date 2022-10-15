ec7dabaad936e000efe33c027399982b2930fa3e
aditokta <115124814+aditokta@users.noreply.github.com>
2022-10-16 01:00:23 +0800

    Add files via upload

---
+++ Modul2/Soal1/PRAK101-2210817110008-Aditya Oktaviari.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=5, y=7, z=9, jumlah = x+y+z;
+    printf("Variabel x bernilai %d\n",x);
+    printf("Variabel y bernilai %d\n",y);
+    printf("Variabel z bernilai %d\n",z);
+    printf("Jumlah variabel tersebut adalah %d",jumlah);
+
+    return 0;
+
+}
