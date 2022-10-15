e12d847a26523098f537d36ebbc9145dcbde23e0
aditokta <115124814+aditokta@users.noreply.github.com>
2022-10-16 00:54:46 +0800

    Add files via upload

---
+++ Modul1/Soal4/PRAK004-2210817110008-Aditya Oktaviari.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("#################################\n\n");
+    printf("#\t\t\t\t#\n\n");
+    printf("#\tAditya Oktaviari\t#\n\n");
+    printf("#\t 2210817110008\t\t#\n\n");
+    printf("#\t\t\t\t#\n\n");
+    printf("#################################");
+
+    return 0;
+}
